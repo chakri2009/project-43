12d7db4446bbf119e7ebb3dab7e3ce67ad6b0198
chakri2009 <75362644+chakri2009@users.noreply.github.com>
2021-05-28 15:25:36 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "BACKGROUND";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("C G B O N D U R K A", 700,150)
+fill("lightblue")
+text("Hint: to clear the screen !!", 700,170)
+
+fill("white")
+    textSize(15)
+    text("E B A A T D A S", 100,250)
+    fill("lightblue")
+    text("Hint: to store the data!!", 100,270)
+
+}
